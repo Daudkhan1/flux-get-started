3c9da43128318c3086ffd61719dba6747e92329d
Daudkhan1 <160847265+Daudkhan1@users.noreply.github.com>
2024-11-14 16:12:57 +0500

    Create variables.tf

---
+++ module/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+    description = " value for the ami"
+    default = ""
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+    default = ""
+}
+
+variable "subnet_id_value" {
+    description = " value for subnet id"
+    default = ""
+}
